69ce1bb6f90a854b0e409ac1c83dedb185290605
Ömer Şükrü Bektaş <omerbkts61@hotmail.com>
2025-07-22 09:52:22 +0300

    Update aztec_watchdog.sh

--- Aztec/aztec_watchdog.sh
+++ Aztec/aztec_watchdog.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SCREEN_NAME="aztec calısan screen adı"
+SCREEN_NAME="aznode"
 AZTEC_CMD=$'aztec start --node --archiver --sequencer --network alpha-testnet --l1-rpc-urls RPC_URL --l1-consensus-host-urls BEACON_URL --sequencer.validatorPrivateKeys 0xYourPrivateKey --sequencer.coinbase 0x381929D0Ce1Ebb278448e1E4061b517B87b60e6F --p2p.p2pIp IP --p2p.maxTxPoolSize 1000000000\n'
 
 while true; do
